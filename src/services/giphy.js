0a6a78726247efa023b21dde13e54fdd54742fad
Yazmin Perez <zoeperez333@gmail.com>
2022-03-22 13:56:29 -0700

    fix indentation & syntax errors

--- src/services/giphy.js
+++ src/services/giphy.js
@@ -1,13 +1,14 @@
 export default class GiphyService {
-  static async getGiphy() {
-    try {
-    let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=bulbasaur&limit=1&offset=0&rating=g&lang=en`)
-    if(!response.ok) {
-      throw Error(response.statusText)
-    }
-    return response.json();
-  } catch (error) {
-    return error.message; 
+  static getGiphy(query) {
+    return await fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${GIPHY_API_KEY}&q=bulbasaur&limit=1&offset=0&rating=g&lang=en`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
     }
   }
-}
--- src/services/poke.js
+++ src/services/poke.js
@@ -1,13 +1,14 @@
 export default class PokeService {
-  static async getPoke(){
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
+  static getPoke(){
+    return fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+      .then(function(response) {
       if(!response.ok) {
         throw Error(response.statusText)
       }
       return response.json();
-    } catch (error) {
+    })
+      .catch(function(error) {
       return error.message;
-    }
+    });
   }
 }
